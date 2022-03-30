a4cd7ead132e5214d4e8300c3821b6b5da272cfa
Luis <acosta88co@gmail.com>
2022-03-30 20:03:22 +0200

    1.00

--- libs/initialSetup.js
+++ libs/initialSetup.js
@@ -1,20 +1,30 @@
 const Role = require("../models/Role");
+const Category = require("../models/Category")
 
 
 const createRoles = async () => {
     try {
+        const countCategory = await Category.estimatedDocumentCount();
         const count = await Role.estimatedDocumentCount();
-        if (count > 0) return;
+        if (count === 0) {
         const values = await Promise.all([
           new Role({name:'client'}).save(),
           new Role({name:'admin'}).save(),
+        ]);}
+        if (countCategory > 0) return;
+        const cats = await Promise.all([
+          new Category({title:'bajo', product_id:[]}).save(),
+          new Category({title:'teclado', product_id:[]}).save(),
+          new Category({title:'percusion', product_id:[]}).save(),
+          new Category({title:'guitarra', product_id:[]}).save(),
+          new Category({title:'viento', product_id:[]}).save(),
         ]);
-       console.log(values);
+
     } catch (error) {
         console.log(error)
         
     }
- 
+   
 }
 
 module.exports = createRoles;
